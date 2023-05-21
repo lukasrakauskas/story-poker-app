9f95650d5ee3aae58da495baaed0c29fe1317d1b
Lukas Rakauskas <lukas.rakauskas@oxylabs.io>
2023-05-21 16:34:01 +0300

    Use port from env

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 
+const port = parseInt(process.env.PORT ?? '4000', 10);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
